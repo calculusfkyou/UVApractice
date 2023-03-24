fb0bb969e09d4cc12d41c673fbf7c1b17ccb2152
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-03-24 23:17:44 +0800

    Create UVA299:Train Swapping.py

---
+++ UVA299:Train Swapping.py
@@ -0,0 +1,20 @@
+def bbsort(subnum):
+	count=0
+	for k in range(len(subnum)-1):
+		for j in range(0,len(subnum)-1):
+			if subnum[j]>subnum[j+1]:
+				temp=subnum[j+1]
+				subnum[j+1]=subnum[j]
+				subnum[j]=temp
+				count+=1
+	return count
+	
+while True:
+	try:
+		n=int(input())
+		for i in range(n):
+			l=int(input())
+			nums=list(map(int,input().split()))
+			print("Optimal train swapping takes %d swaps."%(bbsort(nums)))
+	except EOFError:
+		break
