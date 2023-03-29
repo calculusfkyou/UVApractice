bb361f542298294d18e992d605feed96417aca0a
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-03-30 00:25:34 +0800

    Create UVA10101：Bangla Numbers .py

---
+++ UVA10101：Bangla Numbers .py
@@ -0,0 +1,60 @@
+sequence=1
+while True:
+	try:
+		n=int(input())
+		temp=[]
+		if n==0:
+			temp.append(0)
+		else:
+			while n>0:
+				try:
+					temp2=n%100
+					if temp2!=0:
+						temp.append(temp2)
+					n=n//100
+					if n==0:
+						break
+						
+					temp2=n%10	
+					if temp2!=0:
+						temp.append("shata")
+					if temp2!=0:
+						temp.append(temp2)
+					n=n//10
+					if n==0:
+						break
+						
+					temp2=n%100	
+					if temp2!=0:
+						temp.append("hajar")
+					if temp2!=0:
+						temp.append(temp2)
+					n=n//100
+					if n==0:
+						break
+						
+					temp2=n%100	
+					if temp2!=0:
+						temp.append("lakh")
+					if temp2!=0:
+						temp.append(temp2)
+					n=n//100
+					if n==0:
+						break
+						
+					if n!=0:
+						temp.append("kuti")
+				except:
+					break
+		temp.reverse()
+		if sequence<10:
+			print("   %d. "%(sequence),end="")
+		else:
+			print("  %d. "%(sequence),end="")
+		sequence+=1
+		
+		for i in range(len(temp)-1):
+			print(temp[i],end=" ")
+		print(temp[-1])
+	except EOFError:
+		break
