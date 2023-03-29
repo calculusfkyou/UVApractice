9feba2339654119753e6fe4cf5af67a1da3f607e
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-03-29 22:24:48 +0800

    Create UVA10929：You can say 11 .py

---
+++ UVA10929：You can say 11 .py
@@ -0,0 +1,11 @@
+while True:
+	try:
+		n=int(input())
+		if n==0:
+			break
+		if n%11==0:
+			print("%d is a multiple of 11."%(n))
+		else:
+			print("%d is not a multiple of 11."%(n))
+	except:
+		break
