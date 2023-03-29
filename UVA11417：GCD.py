12d922985d02b71febebe0e74e71a1c5609602d1
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-03-29 21:48:05 +0800

    Create UVA11417：GCD.py

---
+++ UVA11417：GCD.py
@@ -0,0 +1,13 @@
+import math
+while True:
+	try:
+		n=int(input())
+		if n==0:
+			break
+		g=0
+		for i in range(1,n):
+			for j in range(i+1,n+1):
+				g+=math.gcd(i,j)
+		print(g)
+	except EOFError:
+		break
