7b1a342881a28cb69d8a2c9959bb5d46bae28bd7
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-03-31 22:31:57 +0800

    UVA10420：List of Conquests

---
+++ CPE-1star/UVA10420-List of Conquests .py
@@ -0,0 +1,15 @@
+while True:
+	try:
+		n=int(input())
+		England,Spain=0,0
+		for i in range(n):
+			country,woman,last=input().split()
+			if country=="England":
+				England+=1
+			else:
+				Spain+=1
+		print("England %d"%(England))
+		print("Spain %d"%(Spain))	
+			
+	except:
+		break
