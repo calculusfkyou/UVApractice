5a00e389674e886adb84e4a1dd6992fa5c4fd67f
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-04-07 00:03:28 +0800

    UVA10221：Satellites

---
+++ CPE-1star/UVA10221-Satellites .py
@@ -0,0 +1,15 @@
+import math
+s,distance=0,0
+while True:
+	try:
+		r,degree,unit=input().split()
+		r=int(r)
+		degree=int(degree)
+		if unit=="min":
+			degree=degree/60
+		s=2*math.pi*(r+6440)*(degree/360)
+		distance=2*(r+6440)*math.sin(degree*math.pi/180/2)
+		print("%.6f %.6f"%(round(s,6),round(distance,6)))
+		s,distance=0,0
+	except EOFError:
+		break
