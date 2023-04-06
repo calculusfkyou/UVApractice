3d1deffa7526c17f3d8c0589e9a24b36fc6b1cac
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-04-06 23:42:54 +0800

    UVA10268：498'

---
+++ CPE-1star/UVA10268-498' .py
@@ -0,0 +1,14 @@
+while True:
+	try:
+		x=int(input())
+		nums=list(map(int,input().split()))
+		n=len(nums)-1
+		subn=n-1
+		ans=0
+		for i in range(len(nums)):
+			ans+=n*(nums[i]*(x**subn))
+			n-=1
+			subn-=1
+		print(int(ans))
+	except EOFError:
+		break
