b1cf0344bf8bb1f76fca38182e8822e66ec85fee
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-04-06 23:57:02 +0800

    UVA11461：Square Numbers

---
+++ CPE-1star/UVA11461-Square Numbers .py
@@ -0,0 +1,14 @@
+import math
+while True:
+	try:
+		a,b=map(int,input().split())
+		if a==0 and b==0:
+			break
+		count=0
+		for i in range(a,b+1):
+			temp=math.sqrt(i)
+			if temp-int(temp)==0:
+				count+=1
+		print(count)
+	except:
+		break
