579cc2848e29b301730b694d9c8c9c12391e49d7
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-04-07 09:07:23 +0800

    UVA118：Mutant Flatworld Explorers

---
+++ CPE-1star/UVA118-Mutant Flatworld Explorers .py
@@ -0,0 +1,62 @@
+def direction(instruction,side):
+	if side=="E":
+		if instruction=="R":
+			return "S"
+		elif instruction=="L":
+			return "N"
+	elif side=="W":
+		if instruction=="R":
+			return "N"
+		elif instruction=="L":
+			return "S"
+	elif side=="N":
+		if instruction=="R":
+			return "E"
+		elif instruction=="L":
+			return "W"
+	elif side=="S":
+		if instruction=="R":
+			return "W"
+		elif instruction=="L":
+			return "E"
+
+x,y=map(int,input().split())
+world= [[1 for i in range(x)] for j in range(y)]
+while True:
+	try:
+		initialX,initialY,side=input().strip(" ").split()
+		instructions=input()
+		initialX=int(initialX)
+		initialY=int(initialY)
+		subX,subY,check,subside=0,0,0,""
+		for i in range(len(instructions)):
+			if instructions[i]!=" ":
+				subside=side
+				if instructions[i]=="R" or instructions[i]=="L":
+					side=direction(instructions[i],side)
+				else:
+					subX=initialX
+					subY=initialY
+					if side=="E":
+						initialX+=1
+					elif side=="W":
+						initialX-=1
+					elif side=="N":
+						initialY+=1
+					elif side=="S":
+						initialY-=1
+				if initialX>x or initialX<0 or initialY>y or initialY<0:
+					if world[subX-1][subY-1]!=0:
+						world[subX-1][subY-1]=0
+						check=1
+						break
+					else:
+						initialX=subX
+						initialY=subY
+					
+		if check==1:
+			print("%d %d %s LOST"%(subX,subY,subside))
+		else:
+			print("%d %d %s"%(initialX,initialY,side))
+	except EOFError:
+		break
