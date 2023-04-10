dd513c373344173ada0381ea21eb3ae01fad8699
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-04-10 21:22:46 +0800

    UVA10019：Funny Encryption Method

---
+++ CPE-1star/UVA10019-Funny Encryption Method .py
@@ -0,0 +1,25 @@
+while True:
+	try:
+		n=int(input())
+		for i in range(n):
+			m=int(input())
+			subm=m
+			bs=0
+			hm=0
+			count=0
+			while subm>0:
+				if subm%2==1:
+					count+=1
+				subm=subm//2
+			print(count,end=" ")
+			count=0
+			while m>0:
+				temp=m%10
+				while temp>0:
+					if temp%2==1:
+						count+=1
+					temp=temp//2
+				m=m//10
+			print(count)
+	except EOFError:
+		break
