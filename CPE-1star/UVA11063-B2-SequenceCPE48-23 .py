47f7ffb65b94bc69171e259c5fbdf199e029ae93
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-04-10 09:50:26 +0800

    OKay

--- CPE-1star/UVA11063-B2-SequenceCPE48-23 .py
+++ CPE-1star/UVA11063-B2-SequenceCPE48-23 .py
@@ -3,24 +3,22 @@ no=1
 while True:
 	try:
 		n=input()
-		if n==" ":
-			pass
+		nums=list(map(int,input().split()))
+		temp=[]
+		for i in range(len(nums)):
+			for j in range(i+1,len(nums)):
+				temp.append(nums[i]+nums[j])
+		check=0
+		for i in range(len(temp)):
+			count=temp.count(temp[i])
+			if count>1:
+				check+=1
+		if check!=0:
+			print("Case #%d: It is not a B2-Sequence."%(no))
 		else:
-			nums=list(map(int,input().split()))
-			temp=[]
-			for i in range(len(nums)):
-				for j in range(i+1,len(nums)):
-					temp.append(nums[i]+nums[j])
-			check=0
-			for i in range(len(temp)):
-				count=temp.count(temp[i])
-				if count>1:
-					check+=1
-			if check!=0:
-				print("Case #%d: It is not a B2-Sequence."%(no))
-			else:
-				print("Case #%d: It is a B2-Sequence."%(no))
-			print()
-			no+=1
+			print("Case #%d: It is a B2-Sequence."%(no))
+		print()
+		no+=1
+		space=input()#注意這該死的空格
 	except EOFError:
 		break
