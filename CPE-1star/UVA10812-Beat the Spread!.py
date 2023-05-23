ca6a965d6cb94a7398cb314d9007632b533eb213
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-05-23 16:03:58 +0800

    rewrite

--- CPE-1star/UVA10812-Beat the Spread!.py
+++ CPE-1star/UVA10812-Beat the Spread!.py
@@ -11,3 +11,17 @@ while True:
 				print((s-d)//2)				
 	except:
 		break
+
+#2
+while True:
+	try:
+		n=int(input())
+		for i in range(n):
+			s,d=map(int,input().split())
+			if s<d or (s-d)%2!=0:
+				print("impossible")
+			else:
+				print((s+d)//2,end=" ")
+				print((s-d)//2)
+	except:
+		break
