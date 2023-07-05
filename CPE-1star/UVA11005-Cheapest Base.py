0e0ce42fbad84a08f6a64233d9e03cfcc0e78037
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-07-06 00:57:34 +0800

    UVA11005：Cheapest Base 瘋狂程設測資格式有誤

---
+++ CPE-1star/UVA11005-Cheapest Base.py
@@ -0,0 +1,35 @@
+case=1
+n=int(input())
+for c in range(n):
+	try:
+		print("Case %d:"%(case))
+		cost=[]
+		for i in range(4):
+			num=list(map(int,input().split()))
+			cost.extend(num) 
+		line=int(input())
+		for i in range(line):
+			x=int(input())
+			cost2=[]
+			for base in range(2,37):
+				total=0
+				temp=x
+				while temp>0:
+					re=temp%base
+					total=total+cost[re]
+					temp=temp//base
+				cost2.append([base,total])
+			cost2.sort(key=lambda cost2:cost2[1])
+			print("Cheapest base(s) for number %d:"%(x),end="")
+			minimum=cost2[0][1]
+			for base in range(len(cost2)):
+				if cost2[base][1]==minimum:
+					print(" %d"%(cost2[base][0]),end="")
+				else:
+					break
+			print()
+		if case<n:
+			print()
+		case+=1
+	except EOFError:
+		break
