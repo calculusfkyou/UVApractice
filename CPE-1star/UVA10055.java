1d3cc51dbe8a8bc10cc91250baa71a1326aaa9e3
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-07-27 02:33:51 +0800

    1

--- CPE-1star/UVA10055.java
+++ CPE-1star/UVA10055.java
@@ -2,19 +2,20 @@ import java.util.Scanner;
 
 public class UVA10055 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            try {
-                String input = scanner.nextLine();
-                if (input.isEmpty()) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                try {
+                    String input = scanner.nextLine();
+                    if (input.isEmpty()) {
+                        break;
+                    }
+                    String[] inputs = input.split(" ");
+                    int n = Integer.parseInt(inputs[0]);
+                    int m = Integer.parseInt(inputs[1]);
+                    System.out.println(Math.abs(m - n));
+                } catch (Exception e) {
                     break;
                 }
-                String[] inputs = input.split(" ");
-                int n = Integer.parseInt(inputs[0]);
-                int m = Integer.parseInt(inputs[1]);
-                System.out.println(Math.abs(m - n));
-            } catch (Exception e) {
-                break;
             }
         }
     }
