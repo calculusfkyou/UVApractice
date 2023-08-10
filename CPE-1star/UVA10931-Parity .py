9e61742cf4b4ad7616554a01f1f5839a6f21600e
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-08-10 23:56:12 +0800

    V2

--- CPE-1star/UVA10931-Parity .py
+++ CPE-1star/UVA10931-Parity .py
@@ -16,3 +16,15 @@ while True:
 		print("The parity of %d is %d (mod 2)."%(temp,count))
 	except:
 		break
+
+# V2
+while True:
+	try:
+		i=int(input())
+		if i==0:
+			break
+		b=bin(i)[2:]
+		c=b.count("1")
+		print("The parity of %s is %d (mod 2)."%(b,c))
+	except:
+		break
