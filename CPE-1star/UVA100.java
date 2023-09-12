fa4dc7545a56ffd7638e1db34db145c9d85fe502
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-09-13 00:04:22 +0800

    revised

--- CPE-1star/UVA100.java
+++ CPE-1star/UVA100.java
@@ -1,53 +1,49 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Collections;
 
 public class UVA100 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        while (true){
-            try{
-                ArrayList<Integer> temp = new ArrayList<Integer>();
-                ArrayList<Integer> temp2 = new ArrayList<Integer>();
-                int n = scanner.nextInt();
-                int m = scanner.nextInt();
-                int subn = n, subm = m;
-                if (n > m){
-                    int tempp = n;
-                    n = m;
-                    m = tempp;
-                }
-                for (int i = n; i <= m; i++){
-                    if (i == 1){
-                        continue;
-                    }
-                    else{
-                        temp.add(i);
-                        while (true){
-                            if (i ==1)
-                                break;
-                            else{
-                                if (i % 2 != 0){
-                                    i = i * 3 + 1;
-                                    temp.add(i);
-                                }
-                                else{
-                                    i = i / 2;
-                                    temp.add(i);
-                                }
-                            }
-                        }
-                        temp2.add(temp.size());
-                        temp.clear();
-                    }
-                }
-                System.out.printf("%d %d %d\n",subn,subm,Collections.max(temp2));
-                temp2.clear();
+
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
+            int subn = n;
+            int subm = m;
+
+            if (n > m) {
+                int tempp = n;
+                n = m;
+                m = tempp;
             }
-            catch (Exception e){
-                break;
+
+            int[] temp2 = new int[m - n + 1];
+
+            for (int i = n; i <= m; i++) {
+                int num = i;
+                int steps = 1;
+
+                while (num != 1) {
+                    if (num % 2 != 0) {
+                        num = num * 3 + 1;
+                    } else {
+                        num = num / 2;
+                    }
+                    steps++;
+                }
+
+                temp2[i - n] = steps;
             }
+
+            int maxTemp2 = 0;
+            for (int value : temp2) {
+                if (value > maxTemp2) {
+                    maxTemp2 = value;
+                }
+            }
+
+            System.out.printf("%d %d %d%n", subn, subm, maxTemp2);
         }
+
         scanner.close();
     }
 }
