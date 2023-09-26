8043bd1cf465c6b3a484aaa037fae9b50d39136c
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-09-26 11:47:24 +0800

    re

--- UVA1583.py
+++ UVA1583.py
@@ -1,19 +1,20 @@
-#瘋狂程設通過，UVA 超時不通過
+# 瘋狂程設通過，UVA 超時不通過
 def sum_of_digits(s):
     total = 0
     for char in s:
-        if char.isdigit():  
-            total += int(char)  
+        if char.isdigit():
+            total += int(char)
     return total
-    
-t=int(input())
+
+
+t = int(input())
 for i in range(t):
-	n=int(input())
-	check=0
-	for j in range(n):
-		if (j+sum_of_digits(str(j)))==n:
-			print(j)
-			check=1
-			break
-	if not check:
-		print(0)
+    n = int(input())
+    check = 0
+    for j in range(n):
+        if (j + sum_of_digits(str(j))) == n:
+            print(j)
+            check = 1
+            break
+    if not check:
+        print(0)
