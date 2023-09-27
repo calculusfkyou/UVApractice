8e6d145143e4a1331ae2a9d83e6f98e14e34672c
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-09-27 14:21:02 +0800

    re

--- CPE-1star/UVA10041.java
+++ CPE-1star/UVA10041.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class UVA10041 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int testCase = scanner.nextInt();
+public class UVA10041 { // 這個程式的主要功能是計算一系列街區的中位數（median）與每個街區到中位數的距離總和
+    public static void main(String[] args) { // 主程式
+        Scanner scanner = new Scanner(System.in); // 輸入
+        int testCase = scanner.nextInt(); // 題目輸入，有幾筆測資
         for (int i = 0; i < testCase; i++) {
-            int relatives = scanner.nextInt();
-            int[] streetNumber = new int[relatives];
+            int relatives = scanner.nextInt(); // 第一個數表示後面有幾個親戚
+            int[] streetNumber = new int[relatives]; // 宣告已知大小的整數陣列
             for (int j = 0; j < relatives; j++) {
-                streetNumber[j] = scanner.nextInt();
+                streetNumber[j] = scanner.nextInt(); // 輸入親戚家的 street number
             }
-            Arrays.sort(streetNumber);
-            int length = streetNumber.length;
+            Arrays.sort(streetNumber); // 將此陣列排序
             int medianDistance = 0;
-            if (length % 2 == 0) {
-                medianDistance = (streetNumber[length / 2] + streetNumber[length / 2 - 1]) / 2;
+            if (relatives % 2 == 0) { // 取此陣列的中位數，變數 relatives 已知為陣列長度，故直接使用
+                medianDistance = (streetNumber[relatives / 2] + streetNumber[relatives / 2 - 1]) / 2;
+                // 陣列長度為偶數，中位數為中間兩個相加除 2
             } else {
-                medianDistance = streetNumber[length / 2];
+                medianDistance = streetNumber[relatives / 2]; // 陣列長度為奇數，中位數為中間那一個數
             }
             int sumOfDistance = 0;
-            for (int j = 0; j < length; j++) {
+            for (int j = 0; j < relatives; j++) { // 遍歷此陣列，算出中位數和每一個親戚家距離的總和
                 sumOfDistance += Math.abs(medianDistance - streetNumber[j]);
             }
-            System.out.println(sumOfDistance);
+            System.out.println(sumOfDistance); // 輸出總和即為題目所求
         }
-        scanner.close();
+        scanner.close(); // 關閉輸入
     }
 }
