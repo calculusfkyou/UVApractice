fb6ba16ad42fda0a94e18f0ad98ca8e0bfd258a2
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-10-06 15:25:51 +0800

    10062 pass

---
+++ UVA 10000~10099/UVA10062.py
@@ -0,0 +1,26 @@
+def main():
+    check = 1
+    while True:
+        try:
+            text = list(input())
+            if check:
+                check = 0
+            else:
+                print()
+            temp = []
+            used = []
+            for i in range(len(text)):
+                a = 0
+                if text[i] not in used:
+                    used.append(text[i])
+                    a = text.count(text[i])
+                    temp.append([ord(text[i]), a])
+            temp.sort(key=lambda temp: (temp[0]), reverse=1)
+            temp.sort(key=lambda temp: (temp[1]))
+            for i in range(len(temp)):
+                print(*temp[i])
+        except EOFError:
+            break
+
+if __name__=="__main__":
+    main()
