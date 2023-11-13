d4b19b82d0f4a72a755183cd1cc7b4f2d25816f1
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-11-13 22:46:19 +0800

    temp java ver

--- CPE-1star/UVA10190.java
+++ CPE-1star/UVA10190.java
@@ -1,38 +1,42 @@
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
+import java.util.Iterator;
 
 public class UVA10190 {
-    public static void main(String[] args) {
-
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
         while (sc.hasNextInt()) {
-
             long n = sc.nextLong();
             long m = sc.nextLong();
 
-            ArrayList<Long> ans = new ArrayList<>();
-
-            if (m == 0 || m == 1) {
-                System.out.println("Boring!");
-                continue;
-            }
-
+            // Stack因為繼承於Vector，所以Vector的功能也可以使用。
+            Stack<Long> stack = new Stack<Long>(); // Ps也可以使用一般陣列紀錄。
             long temp = 1;
-            while (temp <= n) {
-                ans.add(temp);
-                temp *= m;
+
+            /*
+             * 1. m!=0 : 乘數不為零
+             * 2. m!=1 : 乘數為1，不管乘上甚麼永遠都是1。
+             * 3. n>=m : 因為m^x=n，所以n一定大於等於m。
+             * 4. temp<=n : temp會越乘越大，當超過或等於n時即可停止迴圈。
+             */
+            while (m != 0 && m != 1 && n >= m && temp <= n) {
+                stack.push(temp);
+                temp = temp * m;
             }
 
-            if (ans.get(ans.size() - 1) == n) {
-                for (int i = ans.size() - 1; i >= 0; i--) {
-                    if (i != 0) {
-                        System.out.print(ans.get(i) + " ");
-                    } else {
-                        System.out.print(ans.get(i));
-                    }
+            // Output
+            Iterator<Long> it = stack.iterator();
+            if (!stack.empty() && stack.lastElement() == n) {
+                while (true) {
+                    long Stemp = stack.pop();
+                    System.out.print(Stemp);
+                    if (Stemp != 1)
+                        System.out.print(" ");
+                    else
+                        break;
                 }
-                System.out.println();
+                System.out.println("");
             } else {
                 System.out.println("Boring!");
             }
